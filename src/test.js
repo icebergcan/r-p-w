0271e4158aadd299fa6284ed7489180161983717
icebergcan <aksenmd@yandex.ru>
2022-04-01 17:00:12 +0800

    0104 fin

--- src/test.js
+++ src/test.js
@@ -71,21 +71,7 @@
 
 
 
-console.log("Zss" > "Ass")
+const isFirstLetter = true;
 
-function pow(x, n) {
-    if (n < 0) {
-      console.log("Отрицательные значения 'n' не поддерживаются");
-      return;
-    }
-  
-    let result = 1;
-  
-    for (let i = 0; i < n; i++) {
-      result *= x;
-    }
-  
-    return result;
-  }
-
-console.log(pow(6, 2))
+const a = !"ret";
+console.log(a)
